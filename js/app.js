f16fa888dd3e5604c09bac292e260cc7adb3f16b
unknown <Shikder-Vai>
2022-03-15 19:17:54 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -10,12 +10,34 @@ const showCountries = (countries) => {
   const container = document.getElementById("all-countries");
   container.innerHTML = countriesHtml.join(" ");
 };
-
-const getCountryHtml = (country) => {
+const getCountryHtml = ({ name, flags, capital, area, region }) => {
   return `<div class="country">
+    <img src="${flags.png}" alt="">
+    <h1>Name: ${name.common}</h1>
+    <h2>Capital: ${capital}</h2>
+    <p>Area: ${area}</p>
+    <p>Region: ${region}</p>
+    </div>`;
+};
+
+// option 1.
+// const getCountryHtml = (country) => {
+//   const { name, flags, capital } = country;
+//   return `<div class="country">
+//   <img src="${flags.png}" alt="">
+//   <h1>Name: ${name.common}</h1>
+//   <h2>Capital: ${capital}</h2>
+//   </div>`;
+// };
+
+//Orginal
+/*
+const getCountryHtml = (country) => {
+    return `<div class="country">
     <img src="${country.flags.png}" alt="">
     <h1>Name: ${country.name.common}</h1>
     <h2>Capital: ${country.capital}</h2>
     </div>`;
-};
+  };
+  */
 loadAllCounries();
